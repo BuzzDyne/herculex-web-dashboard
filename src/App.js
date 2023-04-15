d48fc613de6d27db6bb7ef470b39b265415cc09c
BuzzDyne <buzzdyne@gmail.com>
2023-04-15 14:52:12 +0700

    Finished Topbar Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar"
 
-    </div>
-  );
+// 51.57
+
+function App() {
+
+    const [theme, colorMode] = useMode();
+
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider>
+                <CssBaseline />
+                <div className="app">
+                    <main className="content">
+                        <Topbar />
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
