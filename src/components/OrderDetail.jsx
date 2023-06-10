b05f80a0342b7f28dfe55b667b69ded3606e527a
BuzzDyne <buzzdyne@gmail.com>
2023-06-10 16:44:16 +0700

    Implemented OrderItems

--- src/components/OrderDetail.jsx
+++ src/components/OrderDetail.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Paper, Step, StepLabel, Stepper } from '@mui/material';
+import { Box, Button, Divider, Grid, Paper, Step, StepLabel, Stepper, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
@@ -15,8 +15,8 @@ const steps = [
 const OrderDetail = () => {
   const { order_id } = useParams()
   const [order, setOrder] = useState({})
-  // const [activeStep, setActiveStep] = useState(99)
-  // const [numberSubmitted, setNumberSubmitted] = useState(0);
+  const [orderItems, setOrderItems] = useState([{}])
+  // const [numberSubmitted, setNumberSubmitted] = useState(0)
   const [completed, setCompleted] = useState({
     0: false,
     1: false,
@@ -40,7 +40,8 @@ const OrderDetail = () => {
           signal: controller.signal
         })
         console.log(response.data)
-        isMounted && setOrder(response.data)
+        isMounted && setOrder(response.data.order_data)
+        isMounted && setOrderItems(response.data.order_items_data)
 
         const {
           cust_phone_no,
@@ -49,7 +50,7 @@ const OrderDetail = () => {
           print_done_dt,
           packing_done_dt,
           shipped_dt,
-        } = response.data;
+        } = response.data.order_data;
   
         setCompleted({
           0: cust_phone_no !== null && user_deadline_dt !== null,
@@ -107,15 +108,19 @@ const OrderDetail = () => {
     <Grid item xs={9}> {/* Order Informations */}
       <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: '100%'}}>
         <h2>Order #{order.id}</h2>
-        <p>Customer Phone No</p>
-        <p>User Deadline</p>
-        <p>Design Acc Date</p>
-        <p>Print Done Date</p>
+        <p>Customer Phone No: {order.cust_phone_no ? order.cust_phone_no : '-'}</p>
+        <p>User Deadline: {order.user_deadline_dt ? order.user_deadline_dt : '-'}</p>
+        <Grid container spacing={0.5} >
+          <Grid item xs><Button variant="contained" sx={{width:'100%', height:'100%'}}>Input Data</Button></Grid>
+          <Grid item xs><Button variant="contained" sx={{width:'100%', height:'100%'}}>Submit Design</Button></Grid>
+          <Grid item xs><Button variant="contained" sx={{width:'100%', height:'100%'}}>Printing Done</Button></Grid>
+          <Grid item xs><Button variant="contained" sx={{width:'100%', height:'100%'}}>Packing Done</Button></Grid>
+        </Grid>
       </Paper>
     </Grid>
 
 
-    <Grid item xs = {12} sx={{height:'100%', maxHeight:'100px'}}> {/* Stepper */}
+    <Grid item xs={12} sx={{height:'100%', maxHeight:'100px'}}> {/* Stepper */}
         <Stepper activeStep={999} nonLinear alternativeLabel >
           {steps.map((label, index) => (
             <Step key={label} completed={completed[index]}>
@@ -132,10 +137,71 @@ const OrderDetail = () => {
         </Stepper>
     </Grid>
 
-    <Grid item xs={12} > {/* Order Items */}
-      <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-        <h2>Order Items</h2>
-        <p>{order.invoice_ref}</p>
+    <Grid item xs={12}> {/* Order Items */}
+      <Paper sx={{p:2}}>
+      <Typography variant="h5" gutterBottom>Order Items</Typography>
+        <Grid container spacing={2}>
+          {orderItems.map((row) => (
+            <>
+              <Grid item xs={2} sm={2} md={1} lg={1}> {/* Item Image */}
+                <Box 
+                  sx={{
+                    maxWidth: '100%',
+                    width: '100%',
+                    paddingTop: '100%',
+                    position: 'relative',
+                    '& > img': {
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover',
+                    },
+                  }}
+                >
+                    <img src="https://placehold.co/100" alt=""/>
+                </Box>
+              </Grid>
+              <Grid item xs={10} sm={10} md={11} lg={11}> {/* Item Details */}
+                <Typography variant='subtitle1'>{row.product_name}</Typography>
+                <Typography variant='body1'>{row.quantity} x Rp{row.product_price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}</Typography>
+              </Grid>
+            </>
+          ))}
+
+        </Grid>
+      </Paper>
+    </Grid>
+
+    <Grid item xs={12}> {/* Order History */}
+      <Paper sx={{p:2}}>
+        <Typography variant="h5" gutterBottom>Order History</Typography>
+
+        <Paper sx={{p:1, mb:2}}>
+          <Typography variant="body1">Customer Phone Number was set to 021-xxxxxxx and Deadline was set to 2023-Jan-31.</Typography>
+          <Typography variant="body2" gutterBottom>Admin | 2023-01-31 23:59:59</Typography>
+        </Paper>
+
+        <Paper sx={{p:1, mb:2}}>
+          <Typography variant="body1">Customer Phone Number was set to 021-xxxxxxx and Deadline was set to 2023-Jan-31.</Typography>
+          <Typography variant="body2" gutterBottom>Admin | 2023-01-31 23:59:59</Typography>
+        </Paper>
+
+        <Paper sx={{p:1, mb:2}}>
+          <Typography variant="body1">Customer Phone Number was set to 021-xxxxxxx and Deadline was set to 2023-Jan-31.</Typography>
+          <Typography variant="body2" gutterBottom>Admin | 2023-01-31 23:59:59</Typography>
+        </Paper>
+
+        <Paper sx={{p:1, mb:2}}>
+          <Typography variant="body1">Customer Phone Number was set to 021-xxxxxxx and Deadline was set to 2023-Jan-31.</Typography>
+          <Typography variant="body2" gutterBottom>Admin | 2023-01-31 23:59:59</Typography>
+        </Paper>
+
+        <Paper sx={{p:1}}>
+          <Typography variant="body1">Customer Phone Number was set to 021-xxxxxxx and Deadline was set to 2023-Jan-31.</Typography>
+          <Typography variant="body2" gutterBottom>Admin | 2023-01-31 23:59:59</Typography>
+        </Paper>
       </Paper>
     </Grid>
     </>
